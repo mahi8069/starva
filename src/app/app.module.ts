dcf164def9c3fe052233fa4863a928507ed09bee
mahi8069 <mahendrastarva8069@gmail.com>
2021-12-04 01:44:27 +0530

    h

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoucherComponent } from './voucher/voucher.component';
+import { LedgerComponent } from './ledger/ledger.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ReportComponent } from './report/report.component';
+import { CashComponent } from './cash/cash.component';
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoucherComponent,
+    LedgerComponent,
+    InventoryComponent,
+    ReportComponent,
+    CashComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
